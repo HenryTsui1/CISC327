e09dc96d2ade3566f7ba3b2e13dcd811b5267811
Henry Tsui <thehenrytsui@gmail.com>
2020-10-12 19:28:46 -0400

    Cloning CI-Python repo

---
+++ qa327/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import os
+
+"""
+This file defines global variables and config values
+"""
+
+
+package_dir = os.path.dirname(
+    os.path.abspath(__file__)
+)
+
+templates = os.path.join(
+    package_dir, "templates"
+)
+
+app = Flask('this is a simple web application', template_folder=templates)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '69cae04b04756f65eabcd2c5a11c8c24'
+# if the user supplies a database file name, we use
+# that instead, and it should an absolute path
+# for windows user, C:\ is the root directory, so it
+# should not be included. e.g. Users/xxx/Document/db.sqlite
+db_name = os.getenv('DB_NAME')
+if db_name:
+    database_url = "sqlite:////" + db_name
+else:
+    # default:
+    # db.sqlite at the working directory
+    database_url = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
---
+++ qa327/models.py
@@ -0,0 +1,29 @@
+from qa327 import app
+from flask_sqlalchemy import SQLAlchemy
+
+"""
+This file defines all models used by the server
+These models provide us a object-oriented access
+to the underlying database, so we don't need to 
+write SQL queries such as 'select', 'update' etc.
+"""
+
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class User(db.Model):
+    """
+    A user model which defines the sql table
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
+# it creates all the SQL tables if they do not exist
+with app.app_context():
+    db.create_all()
+    db.session.commit()
