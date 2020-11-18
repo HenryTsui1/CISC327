cacef3d432de9e830c7bd8678d12be7742128101
Henry Tsui <thehenrytsui@gmail.com>
2020-11-18 16:03:11 -0500

    Update test_R8.py

--- qa327_test/test_R8.py
+++ qa327_test/test_R8.py
@@ -23,7 +23,7 @@ class R8Test(BaseCase):
         self.open(base_url + '/*') #any other page name that does not exist
         self.assert_element("Error 404 - Page not found.", "#welcome-header")
 
-'''
+
     #R8.2 Make sure that GET and POST work on /register, and verify that error 404 is displayed
     def test_R8_1(self, *_):
         self.open(base_url + '/logout')
@@ -42,7 +42,7 @@ class R8Test(BaseCase):
         self.assert_element("#password")
         self.assert_element("#password2")
         self.click('input[type="submit"]')
-        self.assert_text("Error 404", "#message")
+        self.assert_text("Error 404 - Page not found", "#welcome-header")
 
     #R8.4 Make sure that only GET works on /, and verify that error 404 is displayed
     def test_R8_3(self, *_):
@@ -51,7 +51,7 @@ class R8Test(BaseCase):
         self.type("#email", 'test_frontend@test.com')
         self.type("#password", 'test_frontend')
         self.click('input[type="submit"]')
-        self.assert_text("Error 404", "#message")
+        self.assert_text("Error 404 - Page not found", "#welcome-header")
 
     #R8.5 Make sure that only GET and POST work on /logout
     def test_R8_4(self, *_):
@@ -61,7 +61,7 @@ class R8Test(BaseCase):
         self.type("#password", 'test_frontend')
         self.click('input[type="submit"]')
         self.clic('input[type="logout"]')
-        self.assert_text("Error 404", "#message") #does not direct to /logout, error 404
+        self.assert_text("Error 404 - Page not found", "#welcome-header") #does not direct to /logout, error 404
 
     #R8.6 Make sure that only POST works on /buy
     def test_R8_5(self, *_):
@@ -70,14 +70,13 @@ class R8Test(BaseCase):
         self.type("#email", 'test_frontend@test.com')
         self.type("#password", 'test_frontend')
         self.click('input[type="submit"]')
-        self.assert_text("Error 404", "#message") #Can not access /buy via "/"
+        self.assert_text("Error 404 - Page not found", "#welcome-header") #Can not access /buy via "/", directs to error 404 page
 
-    #R8.7    Make sure that only POST works on /sell
+    #R8.7 Make sure that only POST works on /sell
     def test_R8_6(self, *_):
         self.open(base_url + '/logout')
         self.open(base_url + '/login')
         self.type("#email", 'test_frontend@test.com')
         self.type("#password", 'test_frontend')
         self.click('input[type="submit"]')
-        self.assert_text("Error 404", "#message") #Can not access /sell via "/", directs to error 404 page
-'''
+        self.assert_text("Error 404 - Page not found", "#welcome-header") #Can not access /sell via "/", directs to error 404 page
