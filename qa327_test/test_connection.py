19102337c6501f318fe3da95450757fada9416a3
billyclarke <16wjc1@queensu.ca>
2020-11-18 13:53:59 -0500

    Test R3

--- qa327_test/test_connection.py
+++ qa327_test/test_connection.py
@@ -1,13 +1,13 @@
-import pytest
-import requests
+# import pytest
+# import requests
 
-from qa327_test.conftest import base_url
+# from qa327_test.conftest import base_url
 
-"""
-Simple test case for server connection
-"""
+# """
+# Simple test case for server connection
+# """
 
-@pytest.mark.usefixtures('server')
-def test_server_is_live():
-    r = requests.get(base_url)
-    assert r.status_code == 200
+# @pytest.mark.usefixtures('server')
+# def test_server_is_live():
+#     r = requests.get(base_url)
+#     assert r.status_code == 200
