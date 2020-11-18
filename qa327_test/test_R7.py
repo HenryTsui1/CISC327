64e8d29ac93a51fd1572216c9db6e5561b984132
billyclarke <16wjc1@queensu.ca>
2020-11-18 13:13:28 -0500

    Fixed R7

--- qa327_test/test_R7.py
+++ qa327_test/test_R7.py
@@ -8,17 +8,38 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 # Moch a sample user
 test_user = User(
-    email='test_frontend@test.com',
-    name='test_frontend',
-    password=generate_password_hash('test_frontend')
+    email='test@test.com',
+    name='test_user',
+    password=generate_password_hash('Test!!')
 )
 
+class R7Test(BaseCase):
 
-# class R1Test(BaseCase):
+    @patch('qa327.backend.get_user', return_value=test_user)
 
-#     @patch('qa327.backend.get_user', return_value=test_user)
-#     def test_R1_1(self, *_):
-#         self.open(base_url + '/logout')
-#         self.open(base_url + '/login')
-#         self.assert_element("#login-header")
-#         self.assert_text("Log In", "#login-header")
+    #R7.1 Logout will invalid the current session and redirect to the login page. 
+    def test_R7_1(self, *_):
+        self.open(base_url + '/logout')
+        self.open(base_url + '/login')
+        self.type("#email", 'test@test.com')
+        self.type("#password", 'Test!!')
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+        self.assert_text("Hi" , "#welcome-header")
+        self.open(base_url + '/logout')
+        self.assert_element("#login-header")
+        self.assert_text("Log In", "#login-header")
+
+    #R7.2 After logout, the user shouldn't be able to access restricted pages.
+    def test_R7_2(self, *_):
+        self.open(base_url + '/logout')
+        self.open(base_url + '/login')
+        self.type("#email", 'test@test.com')
+        self.type("#password", 'Test!!')
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+        self.assert_text("Hi" , "#welcome-header")
+        self.open(base_url + '/logout')
+        self.open(base_url + '/')
+        self.assert_element("#login-header")
+        self.assert_text("Log In", "#login-header")
