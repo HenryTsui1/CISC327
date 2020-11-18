ca1395a3414da45a076ed762cb04c7aa17085286
billyclarke <16wjc1@queensu.ca>
2020-11-18 14:06:11 -0500

    Test R3 V3

--- qa327_test/test_R3.py
+++ qa327_test/test_R3.py
@@ -127,8 +127,7 @@ class R3Test(BaseCase):
         self.type("#sell-quantity", "5")
         self.type("#sell-price", "10")
         self.type("#sell-exp", "12/12/2020")
-        self.click("#sell-submit")
-        self.sleep(3)
+        self.click('input[type="submit"]')
         self.assert_element("#message")
         self.assert_text("Sold", "#message")
 
